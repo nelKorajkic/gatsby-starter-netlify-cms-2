0692bae633d8a4966e889b0a0d825805bcdc224f
Nel Korajkic <nelkorajkic@gmail.com>
2018-05-15 16:17:38 -0400

    adding new episode post template w/o testing ¯\_(ツ)_/¯

--- src/templates/episode-post.js
+++ src/templates/episode-post.js
@@ -12,35 +12,100 @@ export const EpisodePostTemplate = ({
   description,
   episodeNumber,
   soundCloudLink,
+  date,
   title,
   helmet
 }) => {
   const PostContent = contentComponent || Content;
 
   return (
-    <section className="section">
-      {helmet || ""}
-      <div className="container content">
+    <div>
+      <div className="container">
         <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            <h1>episode number: {episodeNumber}</h1>
-            <h2>SoundCloud Link: {soundCloudLink}</h2>
-            <iframe
-              width="100%"
-              height="166"
-              scrolling="no"
-              frameborder="no"
-              allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/353421854&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-            />
+          <div
+            className="column 
+            is-8-desktop is-offset-2-desktop 
+            is-8-tablet is-offset-2-tablet
+            is-10-mobile is-offset-1-mobile "
+          >
+            <section>
+              <h1 className="has-text-weight-bold is-size-2 has-text-centered">All Episodes</h1>
+              <div
+                className="episode"
+                style={{
+                  border: "1px solid #eaecee",
+                  padding: "2em 4em",
+                  margin: "2rem auto"
+                }}
+              >
+                <div className="outer">
+                  <svg viewBox="0 0 140 140" preserveAspectRatio="xMidYMin meet">
+                    <defs>
+                      <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" stopColor="#42404A" fillOpacity={1} />
+                        <stop offset="100%" stopColor="#2F65AD" fillOpacity={1} />
+                      </linearGradient>
+                    </defs>
+                    <g>
+                      <circle
+                        r="50%"
+                        cx="50%"
+                        cy="50%"
+                        className="circle-back"
+                        fill="url(#grad1)"
+                      />
+                      <text x="50%" y="50%" textAnchor="middle" dy="0.3em">
+                        E{episodeNumber}
+                      </text>
+                    </g>
+                  </svg>
+                  <span className="episodeDate">{date}</span>
+                </div>
+                <div className="episodeContentContainer">
+                  <p>
+                    <Link className="has-text-primary has-text-bold is-size-4" to="">
+                      {title}
+                    </Link>
+                  </p>
+                  <p className="is-clearfix episodeContent">
+                    {description}
+                    <br />
+                    <br />
+                    <Link className="listenBtn is-pulled-right " to="">
+                      ▶ Listen
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
-      <Player resolveUrl="https://soundcloud.com/ksmtk/chronemics" clientId="" backgroundImage="" />
-    </section>
+    </div>
+
+    //   <section className="section">
+    //   {helmet || ""}
+    //   <div className="container content">
+    //     <div className="columns">
+    //       <div className="column is-10 is-offset-1">
+    //         <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
+    //         <p>{description}</p>
+    //         <PostContent content={content} />
+    //         <h1>episode number: {episodeNumber}</h1>
+    //         <h2>SoundCloud Link: {soundCloudLink}</h2>
+    //         <iframe
+    //           width="100%"
+    //           height="166"
+    //           scrolling="no"
+    //           frameborder="no"
+    //           allow="autoplay"
+    //           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/353421854&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* <Player resolveUrl="https://soundcloud.com/ksmtk/chronemics" clientId="" backgroundImage="" /> */}
+    // </section>
   );
 };
 
@@ -65,6 +130,7 @@ const EpisodePost = ({ data }) => {
       helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
       tags={post.frontmatter.tags}
       title={post.frontmatter.title}
+      date={post.frontmatter.date}
       episodeNumber={post.frontmatter.episodeNumber}
       soundCloudLink={post.frontmatter.soundCloudLink}
     />
